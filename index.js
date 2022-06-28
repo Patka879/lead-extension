be54dae4870fd11a5ebedd5301c1e48a832712e3
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-06-28 15:39:16 +0200

    localStorage set, delete button added

--- index.js
+++ index.js
@@ -4,17 +4,21 @@ let input = document.getElementById("input-el")
 const button = document.getElementById("button-el")
 const ulEl = document.getElementById("ul-el")
 let myLeads = []
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderList()
+}
 
 button.addEventListener("click", function() {
     myLeads.push(input.value)
     input.value = ""
     localStorage.setItem('myLeads', JSON.stringify(myLeads))
     addToTheList()
-    console.log(localStorage.getItem('myLeads'))
 })
 
-function addToTheList() {
+function renderList() {
     let listItem = ""
 
     for (let i = 0; i < myLeads.length; i++) {
