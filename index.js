5be70923b5cb72b9c69f93890b3dddcedf13cd2f
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-06-28 16:56:56 +0200

    added arguments to he functions

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
 "use strict"
 
-let input = document.getElementById("input-el")
+let myLeads = []
+let oldLeads =[]
+const input = document.getElementById("input-el")
 const saveButton = document.getElementById("save-btn")
 const deleteButton = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
-let myLeads = []
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderList()
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItem = ""
+
+    for (let i = 0; i < leads.length; i++) {
+        listItem += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>`
+    }
+    ulEl.innerHTML = listItem
 }
 
 saveButton.addEventListener("click", function() {
     myLeads.push(input.value)
     input.value = ""
     localStorage.setItem('myLeads', JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 })
 
-deleteButton.addEventListener("click", function() {
+deleteButton.addEventListener("dblclick", function() {
     myLeads = []
-    ulEl.innerHTML = ""
-    localStorage.clearItem()
+    localStorage.clear()
+    render(myLeads)
 })
-
-function renderList() {
-    let listItem = ""
-
-    for (let i = 0; i < myLeads.length; i++) {
-        listItem += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>`
-    }
-    ulEl.innerHTML = listItem
-}
